d901b73a4a884fe94c0467854333b3728b37b97a
KL-ML <knataly0513@gmail.com>
2025-04-01 19:32:54 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,23 @@
-// функції для HTTP-запитів
+import axios from 'axios';
+
 const API_KEY = `30636701-b7bfaf1719dc5d89c8acde7b5`;
 const BASE_URL = `https://pixabay.com/api/`;
 
-const searchParams = new URLSearchParams({
+const searchParams = {
   key: API_KEY,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
-});
+  per_page: 15,
+  q: '',
+  page: 1,
+};
 
-function getPhotos(query) {
-  return fetch(`${BASE_URL}?${searchParams}&q=${query}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
+async function getPhotos() {
+  const { data } = await axios.get(BASE_URL, {
+    params: searchParams
   });
+  return data;
 }
 
-export { getPhotos };
+export { getPhotos, searchParams };
