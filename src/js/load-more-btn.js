5f7b134cc443a893bc81607d86e7cbe8de636841
KL-ML <knataly0513@gmail.com>
2025-04-01 20:45:34 +0300

    Update load-more-btn.js

--- src/js/load-more-btn.js
+++ src/js/load-more-btn.js
@@ -1,5 +1,6 @@
 export default class LoadMoreButton {
   static HIDDEN_CLASS = 'hidden';
+  static END_MESSAGE_CLASS = 'end-message';
 
   constructor(buttonEl) {
     this.button = buttonEl;
@@ -19,5 +20,17 @@ export default class LoadMoreButton {
 
   show() {
     this.button.classList.remove(LoadMoreButton.HIDDEN_CLASS);
+  }
+
+  addEndCollectionMessage() {
+    this.button.classList.add(LoadMoreButton.END_MESSAGE_CLASS);
+    this.button.textContent =
+      "We're sorry, but you've reached the end of search results.";
+  }
+
+  removeEndCollectionMessage() {
+    this.button.classList.remove(LoadMoreButton.END_MESSAGE_CLASS);
+    this.button.textContent =
+      "Load more";
   }
 }
