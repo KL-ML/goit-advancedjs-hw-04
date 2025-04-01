6ea4bf9d9fe05812151e2429bc8c2dca9e85e125
KL-ML <knataly0513@gmail.com>
2025-04-01 19:32:59 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
-import { errorAllert } from './js/izi-toasts';
+import { onSubmitForm } from './js/events-handlers';
 import {
   populateInputData,
   setInputDataToLocalStorage,
-  removeFromLS,
 } from './js/locale-storage';
-import { getPhotos } from './js/pixabay-api';
-import {
-  renderGallery,
-  resetGallery,
-  renderLoader,
-  resetLoader,
-} from './js/render-functions';
 
 const searchForm = document.querySelector('.form');
 
@@ -19,47 +11,3 @@ populateInputData(searchForm);
 searchForm.addEventListener('input', setInputDataToLocalStorage);
 searchForm.addEventListener('submit', onSubmitForm);
 
-function onSubmitForm(event) {
-  event.preventDefault();
-  const query = event.currentTarget.elements.searchQuery.value.trim();
-    if (query === '') {
-      errorAllert('Enter your search request');
-    return;
-  }
-
-    resetGallery();
-    renderLoader();
-  getPhotos(query)
-    .then(photos => {
-      if (photos.total === 0) {
-        errorAllert(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-          resetLoader();
-        resetGallery();
-        return;
-      }
-        resetLoader();
-      renderGallery(photos);
-    })
-    .catch(error => {
-      errorAllert(`Error ${error}`);
-    })
-    .finally(() => {
-      searchForm.reset();
-    });
-  event.target.reset();
-  removeFromLS();
-}
-
-// function setInputDataToLocalStorage(event) {
-//   const query = event.target.form.elements.searchQuery.value.trim();
-//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(query));
-// }
-
-// function populateInputData() {
-//   const inputValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-//   if (inputValue) {
-//     searchForm.elements.searchQuery.value = inputValue;
-//   }
-// }
